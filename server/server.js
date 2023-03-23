1b6e8fdff96dd1f7314ab0786b907979b8e0be00
alalboy1230 <calipayanalal@yahoo.com>
2023-03-24 03:21:35 +0800

    initial commit

--- server/server.js
+++ server/server.js
@@ -24,7 +24,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://registar-online-apointment-new.onrender.com",
+    ],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
